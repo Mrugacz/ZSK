672225db36f665d149c718dab1a4af69c63a637c
Mrugacz <mrugalski.kuba@gmail.com>
2020-09-09 10:25:21 +0200

    Added zadania.js

---
+++ PAI2/09.09.20/zadania.js
@@ -0,0 +1,26 @@
+let zad1natychmiastowa = ( function(a) {return a*a;})(20);
+
+function zad1klasyczna(a)
+{
+    return a*a;
+}
+let z1k = zad1klasyczna(20);
+
+function zad2()
+{
+
+}
+
+function zad3()
+{
+
+}
+
+console.log("Executing zad1natychmiastowa:");
+console.log(zad1natychmiastowa);
+console.log("Executing zad1klasyczna:")
+console.log(z1k);
+console.log("Executing zad2:");
+zad2();
+console.log("Executing zad3:");
+zad3();
